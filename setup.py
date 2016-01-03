a23519c46cbc2b95c85c00c6d91f2ae41d355171
hsiao19 <yutunghsaio19@gmail.com>
2016-01-03 21:27:32 +0800

    fixed install bug author=newsletter.__author__, AttributeError: 'module' object has no attribute '__author__'

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from emencia.django import newsletter
 
 
 setup(name='emencia.django.newsletter',
-      version=0.2,
+      version='0.3.dev',
       description='A Django app for sending newsletter by email to a contact list.',
       long_description=open('README.rst').read() + '\n' +
                        open(os.path.join('docs', 'HISTORY.txt')).read(),
@@ -21,11 +21,7 @@ setup(name='emencia.django.newsletter',
           'Development Status :: 5 - Production/Stable',
           'Topic :: Software Development :: Libraries :: Python Modules',],
 
-      author=newsletter.__author__,
-      author_email=newsletter.__email__,
-      url=newsletter.__url__,
-
-      license=newsletter.__license__,
+      license='BSD License',
       packages=find_packages(exclude=['demo']),
       namespace_packages=['emencia', 'emencia.django'],
       include_package_data=True,
